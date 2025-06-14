c93252fac5727441c97500a682f31df7ca6f2f09
Alex Kreskiyan <a.kreskiyan@gmail.com>
2025-06-14 10:32:52 +0300

    Annium.Components: docs

--- shared/test/Annium.Components.State.Forms.Tests/TestBase.cs
+++ shared/test/Annium.Components.State.Forms.Tests/TestBase.cs
@@ -4,8 +4,15 @@ using Xunit;
 
 namespace Annium.Components.State.Forms.Tests;
 
+/// <summary>
+/// Base class for all state forms tests providing common setup and utilities
+/// </summary>
 public abstract class TestBase : Testing.TestBase
 {
+    /// <summary>
+    /// Initializes a new instance of the TestBase class
+    /// </summary>
+    /// <param name="outputHelper">The test output helper</param>
     protected TestBase(ITestOutputHelper outputHelper)
         : base(outputHelper)
     {
@@ -18,7 +25,16 @@ public abstract class TestBase : Testing.TestBase
         });
     }
 
+    /// <summary>
+    /// Gets the state factory instance for creating state containers
+    /// </summary>
+    /// <returns>The state factory instance</returns>
     protected IStateFactory GetFactory() => Get<IStateFactory>();
 
+    /// <summary>
+    /// Gets a validator instance for the specified type
+    /// </summary>
+    /// <typeparam name="T">The type to validate</typeparam>
+    /// <returns>The validator instance</returns>
     protected IValidator<T> GetValidator<T>() => Get<IValidator<T>>();
 }
